c0aadeb3ffa8b2dfd52f41420e4f294eea5f35c3
nk0o0o <94949582+nk0o0o@users.noreply.github.com>
2024-07-08 14:08:31 +0900

    파일리스트 수정/메인&상품뎁스 페이지 html 추가

--- resources/js/pub/include.js
+++ resources/js/pub/include.js
@@ -18,4 +18,46 @@ var include = {
   header: function () {
     document.write(``)
   },
+  floatingSide: function () {
+    document.write(`
+    
+    `);
+  },
+  footer: function () {
+    document.write(`
+      <footer>
+        <div class="inner">
+          <ul class="accord_list">
+            <li class="accord_head">
+              <div class="accord_tit">
+                <div class="logo"><img src="../resources/images/logo.svg" alt="아모레프로페셔널"></div>
+              </div>
+              <div class="accord_cont">
+                <div class="f_cs">
+                  <p>CUSTOMER CENTER</p>
+                  <p class="tel">080-023-0707</p>
+                  <ul>
+                    <li><span>평일</span> 09:00 ~ 18:00</li>
+                    <li><span>점심</span> 12:00 ~ 13:00</li>
+                    <li><span>휴무</span> 주말 및 공휴일</li>
+                  </ul>
+                </div>
+                <div class="f_company">
+                  ㈜ 아모스프로페셔널 / 대표 : 권오창<br>
+                  04386 서울특별시 용산구 한강대로 100 아모레퍼시픽<br>
+                  사업자 등록번호 : 135-81-11122
+                </div>                  
+              </div>
+            </li>
+          </ul>
+          <ul class="f_info">
+            <li><a href="#">회사 소개</a></li>
+            <li><a href="#">서비스 이용약관</a></li>
+            <li><a href="#">개인정보 처리방침</a></li>
+          </ul>
+          <p class="f_copyright">Copyright &copy; <span>아모스프로페셔널.</span> All rights reserved.</p>
+        </div>
+      </footer>
+    `);
+  },
 }
