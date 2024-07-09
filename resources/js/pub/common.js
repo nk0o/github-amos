803d2353d315c71ead3d099454bd1a5bf06cc043
kjs <kimjeongs03@gmail.com>
2024-07-09 10:33:00 +0900

    0709

--- resources/js/pub/common.js
+++ resources/js/pub/common.js
@@ -45,6 +45,7 @@ $(document).ready(function () {
           boxWid = $('.tab_center').outerWidth(),
           boxHalf = $('.tab_center').outerWidth() / 2,
           leftPos = 0,
+          pd = $(el).parent('ul').css('padding-left') * 2,
           selectPos,
           pos;
           $(el).parents('.tab_center').find('.tab_list').each(function() {
@@ -61,6 +62,7 @@ $(document).ready(function () {
       } else {
           pos = selectPos - boxHalf;
       }
+
       $(el).parents('.tab_center').find('>ul').animate({scrollLeft:pos});
     }
   }
