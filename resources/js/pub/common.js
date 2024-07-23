9a2792c705b435692d1ef90fba50a683a0678cc8
kjs <kimjeongs03@gmail.com>
2024-07-23 16:12:47 +0900

    상세페이지 작업

--- resources/js/pub/common.js
+++ resources/js/pub/common.js
@@ -696,12 +696,13 @@ function tabMoveAnchor() {//.tab_menu.clickTab --> .toCont
 //카트 담기
 function putInCart() {
   $('.prd_cart_btn').on('click',function() {
-    $('.appbar .appbar_util').append('<div class="prd_put_cart"><div class="aniPut"> <div class="ani_img"><i class="ico_cart_float ico_24"></i></div></div><div class="put_text"><p>나의 장바구니에 담았습니다</p></div></div>')
-    $('.prd_put_cart').addClass('active');
-    setTimeout(function() {
-      $('.prd_put_cart').removeClass('active');
-      // $('.prd_put_cart').remove();
-    },4500)
+    if($('.prd_put_cart').length == 0) {
+      $('.appbar > .inner').append('<div class="prd_put_cart"><div class="aniPut"> <div class="ani_img"><i class="ico_cart_float ico_24"></i></div></div><div class="put_text"><p>나의 장바구니에 담았습니다</p></div></div>')
+      $('.prd_put_cart').addClass('active');
+      setTimeout(function() {
+        $('.prd_put_cart').removeClass('active').remove();
+      },4500)
+    }
   })
 }
 
@@ -723,13 +724,24 @@ function prdImgFix() {
 
 function tabContPos() {
   let tabContPos = $('.tab_cont').offset().top;
-  let headeH = $('header').height();
-  let tabMenuH = $('.tab_menu').height();
+  let headeH = $('header').outerHeight();
+  let tabMenuH = $('.tab_menu').outerHeight();
   console.log(tabContPos)
   $('.tab_list ').on('click',function() {
-    $('html,body').animate({
-      scrollTop : tabContPos - headeH - tabMenuH
-    })
+    //아이폰 확인
+    function isIphone() {
+      return /iPhone/.test(navigator.userAgent);
+    }
+    if (isIphone()) {
+      $('html,body').animate({
+        scrollTop : tabContPos - headeH - tabMenuH + 20
+      })
+    } else {
+      $('html,body').animate({
+        scrollTop : tabContPos - headeH - tabMenuH
+      })
+    }
   })
+  
 }
 
