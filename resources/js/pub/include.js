3af781d68588f5209657e3980bb4cc42a59978ea
nk0o <skrud041@naver.com>
2024-09-23 17:36:45 +0900

    푸터 수정, 에러페이지작업, 수정작업, 에디터

--- resources/js/pub/include.js
+++ resources/js/pub/include.js
@@ -140,42 +140,45 @@ var include = {
   },
   footer: function () {
     document.write(`
-      <footer>
-        <div class="inner">
-          <ul class="accord_list">
-            <li class="accord_head">
-              <div class="accord_tit">
-                <div class="logo">
-                  <img src="../../resources/images/simbol_2.svg" alt="아모레프로페셔널"><!-- 240920 이미지 수정 -->
+      <!-- 240923 푸터 감싸는 div추가 -->
+      <div class="footer_wrap">
+        <footer>
+          <div class="inner">
+            <ul class="accord_list">
+              <li class="accord_head">
+                <div class="accord_tit">
+                  <div class="logo">
+                    <img src="../../resources/images/simbol_2.svg" alt="아모레프로페셔널"><!-- 240920 이미지 수정 -->
+                  </div>
                 </div>
-              </div>
-              <div class="accord_cont">
-                <div class="f_cs">
-                  <p>CUSTOMER CENTER</p>
-                  <p class="tel">080-023-0707</p>
-                  <ul>
-                    <li><span>평일</span> 09:00 ~ 18:00</li>
-                    <li><span>점심</span> 12:00 ~ 13:00</li>
-                    <li><span>휴무</span> 주말 및 공휴일</li>
-                  </ul>
+                <div class="accord_cont">
+                  <div class="f_cs">
+                    <p>CUSTOMER CENTER</p>
+                    <p class="tel">080-023-0707</p>
+                    <ul>
+                      <li><span>평일</span> 09:00 ~ 18:00</li>
+                      <li><span>점심</span> 12:00 ~ 13:00</li>
+                      <li><span>휴무</span> 주말 및 공휴일</li>
+                    </ul>
+                  </div>
+                  <div class="f_company">
+                    ㈜ 아모스프로페셔널 / 대표 : 권오창<br>
+                    04386 서울특별시 용산구 한강대로 100 아모레퍼시픽<br>
+                    사업자 등록번호 : 135-81-11122
+                  </div>
                 </div>
-                <div class="f_company">
-                  ㈜ 아모스프로페셔널 / 대표 : 권오창<br>
-                  04386 서울특별시 용산구 한강대로 100 아모레퍼시픽<br>
-                  사업자 등록번호 : 135-81-11122
-                </div>                  
-              </div>
-            </li>
-          </ul>
-          <ul class="f_info">
-            <li><a href="#">회사 소개</a></li>
-            <li><a href="#">서비스 이용약관</a></li>
-            <li><a href="#">개인정보 처리방침</a></li>
-            <li><a href="#">입점상담</a></li><!-- 240903 입점상담 추가 -->
-          </ul>
-          <p class="f_copyright">Copyright &copy; <span>아모스프로페셔널.</span> All rights reserved.</p>
-        </div>
-      </footer>
+              </li>
+            </ul>
+            <ul class="f_info">
+              <li><a href="#">회사 소개</a></li>
+              <li><a href="#">서비스 이용약관</a></li>
+              <li><a href="#">개인정보 처리방침</a></li>
+              <li><a href="#">입점상담</a></li><!-- 240903 입점상담 추가 -->
+            </ul>
+            <p class="f_copyright">Copyright &copy; <span>아모스프로페셔널.</span> All rights reserved.</p>
+          </div>
+        </footer>
+      </div>
     `);
   },
   floatingSide: function () {
